3d30d47fa4960930b5c2bbb06b96f46b064a0d8b
hamzabinsarfraz <muhammadameerhmza@gmail.com>
2020-10-23 02:58:42 +0500

    daf remote agent

--- agent.js
+++ agent.js
@@ -1,25 +1,79 @@
-// agent.ts
-const { createAgent, IIdentityManager, IResolver, IKeyManager, IDataStore, IMessageHandler } = require('daf-core');
-const { ICredentialIssuer } = require('daf-w3c');
-const { IDIDComm } = require('daf-did-comm');
-const { ISelectiveDisclosure } = require('daf-selective-disclosure');
-const { IDataStoreORM } = require('daf-typeorm');
+const { createAgent, Agent, IIdentityManager, IResolver, IKeyManager, IDataStore, IMessageHandler } = require('daf-core');
+const { MessageHandler } = require('daf-message-handler');
+const { KeyManager } = require('daf-key-manager');
+const { IdentityManager } = require('daf-identity-manager');
+const { DafResolver } = require('daf-resolver');
+const { JwtMessageHandler } = require('daf-did-jwt');
+const { CredentialIssuer, ICredentialIssuer, W3cMessageHandler } = require('daf-w3c');
+const { EthrIdentityProvider } = require('daf-ethr-did');
+const { WebIdentityProvider } = require('daf-web-did');
+const { DIDComm, DIDCommMessageHandler, IDIDComm } = require('daf-did-comm');
+const { SelectiveDisclosure, ISelectiveDisclosure, SdrMessageHandler } = require('daf-selective-disclosure');
+const { KeyManagementSystem, SecretBox } = require('daf-libsodium');
+const { Entities, KeyStore, IdentityStore, IDataStoreORM, DataStore, DataStoreORM } = require('daf-typeorm');
+const { createConnection } = require('typeorm');
 const { AgentRestClient } = require('daf-rest');
+
+// const infuraProjectId = '5ffc47f65c4042ce847ef66a3fa70d4c'
+// const secretKey = '29739248cad1bd1a0fc4d9b75cd4d2990de535baf5caadfdf8d8f86664aa830c'
+// const databaseFile = 'database.sqlite'
+
+// const dbConnection = createConnection({
+//   type: 'sqlite',
+//   database: databaseFile,
+//   synchronize: true,
+//   logging: false,
+//   entities: Entities,
+// })
+
+// const serverAgent = new Agent({
+//   plugins: [
+//     new KeyManager({
+//       store: new KeyStore(dbConnection, new SecretBox(secretKey)),
+//       kms: {
+//         local: new KeyManagementSystem(),
+//       },
+//     }),
+//     new IdentityManager({
+//       store: new IdentityStore(dbConnection),
+//       defaultProvider: 'did:ethr:rinkeby',
+//       providers: {
+//         'did:ethr:rinkeby': new EthrIdentityProvider({
+//           defaultKms: 'local',
+//           network: 'rinkeby',
+//           rpcUrl: 'https://rinkeby.infura.io/v3/' + infuraProjectId,
+//         }),
+//         'did:web': new WebIdentityProvider({
+//           defaultKms: 'local',
+//         }),
+//       },
+//     }),
+//     new DafResolver({ infuraProjectId }),
+//     new DataStore(dbConnection),
+//     new DataStoreORM(dbConnection),
+//     new MessageHandler({
+//       messageHandlers: [
+//         new DIDCommMessageHandler(),
+//         new JwtMessageHandler(),
+//         new W3cMessageHandler(),
+//         new SdrMessageHandler(),
+//       ],
+//     }),
+//     new DIDComm(),
+//     new CredentialIssuer(),
+//     new SelectiveDisclosure(),
+//   ],
+// });
+
+const supportedMethods = [
+  'identityManagerImportIdentity'
+];
 
 const agent = createAgent({
   plugins: [
     new AgentRestClient({
       url: 'http://localhost:3002/agent',
-      enabledMethods: [
-        'keyManagerCreateKey',
-        'keyManagerGetKey',
-        'keyManagerDeleteKey',
-        'keyManagerImportKey',
-        'keyManagerEncryptJWE',
-        'keyManagerDecryptJWE',
-        'keyManagerSignJWT',
-        'keyManagerSignEthTX',
-      ]
+      enabledMethods: supportedMethods
     })
   ]
 });
--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require('express');
-const { agent } = require('./agent');
+const { serverAgent, agent } = require('./agent');
+const cors = require('cors');
 const { AgentRouter } = require('daf-express');
+const bodyParser = require('body-parser');
+const basePath = '/agent';
+const app = express()
 
 const agentRouter = AgentRouter({
-    getAgentForRequest: async (req, res) => {
-        const agentMethods = await agent.availableMethods();
-        const schema = agent.getSchema()
-        console.log(schema);
-    },
-    exposedMethods: agent.availableMethods()
+    getAgentForRequest: async req => agent,
+    exposedMethods: agent.availableMethods(),
 });
 
-const app = express()
-app.use('/agent', agentRouter)
+// app.use(basePath, agentRouter);
+
+app.post(`${basePath}/did`, agentRouter, async (req, res) => {
+    try {
+        const did = req.body.did;
+        console.log(did);
+        const importIdentity = await agent.identityManagerImportIdentity({ did: did });
+        console.log(importIdentity);
+        res.json({ identity: importIdentity });
+    } catch (error) {
+        console.log(error);
+        res.json({ error: error.message })
+    }
+})
+
 app.listen(3002, () => {
-    console.log('server is live on port 3002! ')
+    console.log('app is live on port 3002');
 });
